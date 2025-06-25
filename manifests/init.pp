5d9f36ae7b75e4b168f407e3b18415cf6b6ce29c
Yorick Poels <yorick.poels@kuleuven.be>
2025-06-25 10:49:53 +0200

    Add support for package install options.
    See puppet package resource for more info:
    https://puppet.com/docs/puppet/7/types/package.html#package-attribute-install_options

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,9 +29,13 @@ class keepalived (
   $service_hasstatus = $::keepalived::params::service_hasstatus,
   $service_restart   = $::keepalived::params::service_restart,
   $package_ensure    = 'installed',
+  Optional[Array[String]] $package_install_options = undef,
 ) inherits ::keepalived::params {
 
-  package { $package: ensure => $package_ensure }
+  package { $package: 
+    ensure => $package_ensure,
+    install_options => $package_install_options,
+  }
 
   service { $service:
     ensure    => $service_ensure,
